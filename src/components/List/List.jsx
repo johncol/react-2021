f39916b04ef35062826e77759d8cbbc9e63d99cb
John <john.19col@gmail.com>
2021-04-21 15:46:29 -0700

    Sort items using useMemo hook

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,14 +1,19 @@
+import { useMemo } from 'react';
 import './List.css';
 
 export const List = ({ title, items, primary }) => {
   const hasItems = items && items.length > 0;
+  const itemsSortedByPriority = useMemo(() => {
+    return items.sort((item1, item2) => item1.priority - item2.priority);
+  }, [items]);
+
   return (
     <section className={`list panel is-${primary ? 'primary' : 'info'}`}>
       <h2 className="panel-heading">{title}</h2>
       <div className="list-content">
         {hasItems ? (
           <ol>
-            {items.map((item) => (
+            {itemsSortedByPriority.map((item) => (
               <li key={item.id}>{item.description}</li>
             ))}
           </ol>
