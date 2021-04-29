02c393c3bff6c0b19e3114ee1fb05544fbe4737a
John <john.19col@gmail.com>
2021-04-28 20:06:05 -0700

    Remove unused useJsonLocalState hook

--- src/hooks/useJsonLocalState.js
+++
@@ -1,9 +0,0 @@
-import { useDebugValue, useState } from 'react';
-import jsonState from './../state.json';
-
-export const useJsonLocalState = () => {
-  const [state, setState] = useState(JSON.parse(JSON.stringify(jsonState)));
-  useDebugValue(state);
-
-  return [state, setState];
-};
