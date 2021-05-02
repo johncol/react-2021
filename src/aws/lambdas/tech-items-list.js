487abfa8288e9e546fa670df1379cf1446eb84e3
John <john.19col@gmail.com>
2021-05-01 20:19:45 -0700

    Add AWS folder with usage file and 1st lambda

---
+++ src/aws/lambdas/tech-items-list.js
@@ -0,0 +1,25 @@
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+const TECH_ITEMS_TABLE = 'tech-items';
+
+// eslint-disable-next-line no-unused-vars
+exports.handler = async (_event, _context) => {
+  let response = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  try {
+    const rawResponse = await dynamo
+      .scan({ TableName: TECH_ITEMS_TABLE })
+      .promise();
+    response.body = JSON.stringify(rawResponse);
+    response.statusCode = 200;
+  } catch (error) {
+    response.body = error.message;
+    response.statusCode = 400;
+  }
+
+  return response;
+};
